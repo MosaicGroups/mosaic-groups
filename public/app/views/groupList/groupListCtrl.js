b746e15049853031ffe9386e23df835210704bfa
Patrick Blair <pblair12@gmail.com>
2014-02-23 08:09:39 -0500

    added buttons for edit and delete of groups for users who have the correct access

--- public/app/views/groupList/groupListCtrl.js
+++ public/app/views/groupList/groupListCtrl.js
@@ -1,3 +1,37 @@
-angular.module('app').controller('groupListCtrl', function($scope, groupService) {
+angular.module('app').controller('groupListCtrl', function($scope, groupService, identityService, notifierService) {
   $scope.groups = groupService.getGroups();
+  $scope.identity = identityService;
+
+  $scope.editGroup = function(group) {
+    notifierService.notify("Not yet implemented");
+  }
+
+  $scope.deleteGroup = function(group) {
+    notifierService.notify("Not yet implemented");
+  }
+
+  $scope.canEdit = function(group) {
+    var canEditGroup = false;
+    if (!identityService.isAuthenticated()) {
+      canEditGroup = false;
+    }
+    else if (identityService.isAdmin()) {
+      canEditGroup = true;
+    }
+    else if ($scope.userIsLeaderOfGroup(identityService.currentUser, group)) {
+      canEditGroup = true;
+    }
+    return canEditGroup;
+  }
+
+  $scope.userIsLeaderOfGroup = function(user, group) {
+    var canEditGroup = false;
+    for (var i = 0; i < group.leaders.length; i++) {
+      var leader = group.leaders[i];
+      if (user._id === leader._id) {
+        canEditGroup = true;
+      }
+    }
+    return canEditGroup;
+  }
 });
