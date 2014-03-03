3700551f325f6b6a2036c368ff5616880107971b
Patrick Blair <pblair12@gmail.com>
2014-03-03 05:44:46 -0500

    list of groups is now sortable

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngResource', 'ngRoute', 'ui.bootstrap']);
+angular.module('app', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ngTable']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
   var routeRoleChecks = {
--- public/app/views/groupList/groupListCtrl.js
+++ public/app/views/groupList/groupListCtrl.js
@@ -1,6 +1,25 @@
-angular.module('app').controller('groupListCtrl', function($scope, $location, groupService, identityService, notifierService) {
-  $scope.groups = groupService.getGroups();
+angular.module('app').controller('groupListCtrl', function($scope, $location, $filter, ngTableParams, groupService, identityService, notifierService) {
   $scope.identity = identityService;
+
+  $scope.tableParams = new ngTableParams({
+    page: 1,            // show first page
+    count: 10,          // count per page
+    sorting: {
+      title: 'asc'     // initial sorting
+    }
+  }, {
+    total: 0, // length of $scope.groups
+    getData: function($defer, params) {
+      groupService.getGroups().$promise.then(function(data) {
+        params.total(data.total);
+        var orderedData =
+          params.sorting() ?
+            $filter('orderBy')(data, params.orderBy()) :
+            data;
+        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+      });
+    }
+  });
 
   $scope.joinGroup = function(group) {
     $location.path('/views/groupJoin/group-join/' + group._id);
