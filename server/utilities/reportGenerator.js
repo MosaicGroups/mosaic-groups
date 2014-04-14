2db6dd1c1c07214552716f0d06a9e1ba39c2e54e
Patrick Blair <pblair12@gmail.com>
2014-04-14 14:42:32 -0400

    fixing the update user password feature

--- server/utilities/reportGenerator.js
+++ server/utilities/reportGenerator.js
@@ -2,6 +2,7 @@ exports.createDailyReport = function(groups) {
   var today = new Date();
   var yesterday = new Date();
   yesterday.setDate(today.getDate()-1);
+  var numMembers = 0;
   var numNewMembers = 0;
   var reportHtml = "", membersHtml = "";
 
@@ -11,7 +12,13 @@ exports.createDailyReport = function(groups) {
   membersHtml += "<ul>";
   for (var i = 0; i < groups.length; i++) {
     var group = groups[i];
-    membersHtml += "<li>" + group.title + " on " + group.dayOfTheWeek + " in " + group.location + " lead by: ";
+    membersHtml += "<li>";
+    if (group.members.length >= group.memberLimit) {
+      membersHtml += "(FULL) ";
+    }
+    membersHtml += group.title + " on " + group.dayOfTheWeek + " in " + group.location;
+    membersHtml += " with " + group.members.length + " members";
+    membersHtml += " lead by: ";
     for (var j = 0; j < group.leaders.length; j++) {
       var leader = group.leaders[j];
       membersHtml += leader.firstName + " " + leader.lastName + " &lt;" + leader.username + "&gt; ";
@@ -21,6 +28,7 @@ exports.createDailyReport = function(groups) {
 
     membersHtml += "<ul>";
     for (var j = 0; j < group.members.length; j++) {
+      numMembers++;
       var member = groups[i].members[j];
       var memberHtml = member.firstName + " " + member.lastName + " &lt;" +  member.email + "&gt; " + member.joinDate.toDateString() + "</li>";
       if (member.joinDate > yesterday) {
@@ -33,6 +41,7 @@ exports.createDailyReport = function(groups) {
     membersHtml += "</ul>";
   }
   membersHtml += "</ul>";
+  reportHtml += "<p>There are " + numMembers + " members signed up for growth groups (if a person signed up for 2 or more groups they are counted as 2 or more)</p>";
   reportHtml += "<p>There were <span style='color:green'>" + numNewMembers + "</span> new members in the past 24hours";
   reportHtml += membersHtml;
   return reportHtml;
