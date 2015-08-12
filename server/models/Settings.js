e72329a46312b978b7a7c6db82c92f7c02770fef
Jeffrey Klassen <jeff.klassen@siginttech.com>
2015-08-12 09:26:05 +0200

    added dates message to header jumbotron

--- server/models/Settings.js
+++ server/models/Settings.js
@@ -1,32 +1,38 @@
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 var settingsSchema = mongoose.Schema({
-  disableGroups: {
-    type: Boolean,
-    default: false
-  },
-  showNextSemesterMsg: {
-    type: Boolean,
-    default: false
-  },
-  nextSemesterMsg: {
-    type: String,
-    default: 'Next Semester Growth Groups Coming Soon...'
-  }
+    disableGroups: {
+        type: Boolean,
+        default: false
+    },
+    showNextSemesterMsg: {
+        type: Boolean,
+        default: false
+    },
+    nextSemesterMsg: {
+        type: String,
+        default: 'Next Semester Growth Groups Coming Soon...'
+    },
+    datesMsg: {
+        type: String,
+        default: ''
+    }
 });
 
 var Settings = mongoose.model('Settings', settingsSchema);
 
 function createDefaultSettings() {
-  Settings.find({}).exec(function(err, collection) {
-    if(collection.length === 0) {
-      console.log("creating default settings");
-      Settings.create({disableGroups:false});
-    } else {
-      console.log("not creating default settings because they are already created");
-    }
-  })
+    Settings.find({}).exec(function (err, collection) {
+        if (collection.length === 0) {
+            console.log("creating default settings");
+            Settings.create({
+                disableGroups: false
+            });
+        } else {
+            console.log("not creating default settings because they are already created");
+        }
+    })
 };
 
 exports.createDefaultSettings = createDefaultSettings;
