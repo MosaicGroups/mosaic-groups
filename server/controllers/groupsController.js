a0b6238780530582eb0f60aa4d81c10f3a4f6d13
Patrick Blair <pblair12@gmail.com>
2016-01-13 17:48:41 -0500

    fixing error in emails #38, also cleaning up code a bit

--- server/controllers/groupsController.js
+++ server/controllers/groupsController.js
@@ -1,88 +1,91 @@
 var logger = require('../config/logger');
 var Group = require('mongoose').model('Group'),
-    User = require('mongoose').model('User'),
-    Member = require('mongoose').model('Member'),
-    Settings = require('mongoose').model('Settings'),
-    errorHandler = require('../utilities/errorHandler'),
+User = require('mongoose').model('User'),
+Member = require('mongoose').model('Member'),
+Settings = require('mongoose').model('Settings'),
+errorHandler = require('../utilities/errorHandler'),
 
-    emailer = require('../utilities/emailer'),
-    groupsService = require('../services/groupsService');
+emailer = require('../utilities/emailer'),
+groupsService = require('../services/groupsService');
 
 exports.emailGroupReportToSelf = function (req, res) {
-    var user = req.user;
-    groupsService.emailGroupReportToSelf(user, function () { });
+  var user = req.user;
+  groupsService.emailGroupReportToSelf(user, function () {
+  });
 
-    return res.end();
+  return res.end();
 };
 
 exports.emailUniqueReportToSelf = function (req, res) {
-    var user = req.user;
-    groupsService.emailUniqueReportToSelf(user, function () { });
-    
-    return res.end();
+  var user = req.user;
+  groupsService.emailUniqueReportToSelf(user, function () {
+  });
+
+  return res.end();
 };
 
 
 exports.getGroups = function (req, res) {
-    groupsService.getGroups(function (err, collection) {
-        if (err) {
-            errorHandler.sendError(req, res, err);
-        } else {
-            res.send(collection);
-        }
-    });
+  groupsService.getGroups(function (err, collection) {
+    if (err) {
+      errorHandler.sendError(req, res, err);
+    } else {
+      res.send(collection);
+    }
+  });
 };
 
 exports.getGroup = function (req, res) {
-    var groupId = req.params.id;
-    groupsService.getGroup(groupId, function (err, group) {
-        if (err) {
-            errorHandler.sendError(req, res, err);
-        } else {
-            res.send(group);
-        }
-    });
+  var groupId = req.params.id;
+  groupsService.getGroup(groupId, function (err, group) {
+    if (err) {
+      errorHandler.sendError(req, res, err);
+    } else {
+      res.send(group);
+    }
+  });
 };
 
 exports.saveGroup = function (req, res, next) {
-    var groupData = req.body;
+  var groupData = req.body;
 
-    // if the leaderId is not set, or if this is not an admin user
-    // then set the leaderId to the current user
-    if (!groupData.leaders || groupData.leaders.length <= 0 || !req.user.hasRole('admin')) {
-        groupData.leaders = [req.user._id];
+  // if the leaderId is not set, or if this is not an admin user
+  // then set the leaderId to the current user
+  if (!groupData.leaders || groupData.leaders.length <= 0 || !req.user.hasRole('admin')) {
+    groupData.leaders = [req.user._id];
+  }
+
+  groupsService.saveGroup(groupData, function (err, group) {
+    if (err) {
+      errorHandler.sendError(req, res, err);
+    } else {
+      emailer.sendAuditMessageEMail('Group: "' + groupData.title + '" was created by: ' + req.user.username);
+      res.send(group);
     }
-
-   groupsService.saveGroup(groupData,function(err,group){
-        if (err) {
-            errorHandler.sendError(req, res, err);
-        } else {
-            emailer.sendAuditMessageEMail('Group: "' + groupData.title + '" was created by: ' + req.user.username);
-            res.send(group);
-        }
-    })
+  })
 };
 
 exports.updateGroup = function (req, res) {
-    var groupUpdates = req.body;
-    var groupId = groupUpdates._id;
-    delete groupUpdates["_id"];
+  var groupUpdates = req.body;
+  var groupId = groupUpdates._id;
+  delete groupUpdates["_id"];
 
-    // if this is not an admin then the group must contain the current user as one of its leaders
-    if (!req.user.hasRole('admin') && groupUpdates.leaders.indexOf(req.user.id) < 0) {
-        res.status(403);
-        return res.end();
+  // if this is not an admin then the group must contain the current user as one of its leaders
+  if (!req.user.hasRole('admin') && groupUpdates.leaders.indexOf(req.user.id) < 0) {
+    res.status(403);
+    return res.end();
+  }
+
+  groupsService.updateGroup(groupId, groupUpdates, function (err) {
+    if (err) {
+      errorHandler.sendError(req, res, err);
+    } else {
+      groupUpdates._id = groupId;
+      emailer.sendAuditMessageEMail('Group: "' + groupUpdates.title + '" was updated by: ' + req.user.username);
+      res.send(groupUpdates);
     }
+  });
 
-    groupsService.updateGroup(groupId, groupUpdates, function (err) {
-        if (err) {
-            errorHandler.sendError(req, res, err);
-        } else {groupUpdates._id = groupId;
-            emailer.sendAuditMessageEMail('Group: "' + groupUpdates.title + '" was updated by: ' + req.user.username);
-            res.send(groupUpdates);
-        }
-    });
-   
 };
 
 /**
@@ -96,32 +99,24 @@ exports.updateGroup = function (req, res) {
  * @param res
  */
 exports.addMember = function (req, res) {
-    var memberData = req.body.newMember;
-    memberData.status = "PENDING";
-    memberData.joinDate = new Date();
+  var memberData = req.body.newMember;
+  memberData.status = "PENDING";
+  memberData.joinDate = new Date();
 
-    var groupId = req.params.id
-    var userIsAuthenticated = req.isAuthenticated();
+  var groupId = req.params.id
+  var userIsAuthenticated = req.isAuthenticated();
 
-    groupsService.addMember(groupId, memberData, userIsAuthenticated, function (err) {
-        errorHandler.sendError(req, res, err);
-    }, function (err, group) {
-        emailer.sendAddedMemberEMail(groupId, memberData, function (err, response) {
-            if (err) {
-                logger.log('Unable to send email', err);
-                return res.end();
-            } else {
-                emailer.sendMemberConfirmationEmail(group, memberData, function (err, response) {
-                    if (err) {
-                        logger.log('Unable to send email', err);
-                        return res.end();
-                    } else {
-                        return res.end();
-                    }
-                });
-            }
-        });
+  groupsService.addMember(groupId, memberData, userIsAuthenticated, function (err) {
+    errorHandler.sendError(req, res, err);
+  }, function (err, group) {
+    emailer.sendAddedMemberEMail(group, memberData, function (err, response) {
+      if (err) { logger.log('Unable to send email that a member was added', err); }
+      emailer.sendMemberConfirmationEmail(group, memberData, function (err, response) {
+        if (err) { logger.log('Unable to send email for group add confirmation', err); }
+      });
     });
+    return res.end();
+  });
 };
 
 /**
@@ -130,23 +125,23 @@ exports.addMember = function (req, res) {
  * @param res
  */
 exports.deleteGroup = function (req, res) {
-    // get the group object from the request body that is to be deleted
-    var groupDeleteId = req.params.id;
+  // get the group object from the request body that is to be deleted
+  var groupDeleteId = req.params.id;
 
-    // only admins can delete groups
-    if (!req.user || !req.user.hasRole('admin')) {
-        errorHandler.sendError(req, res, new Error('only admin users may delete a group'), 403);
-    }
-    // otherwise, get the group from the database then delete them
-    else {
-        groupsService.deleteGroup(groupDeleteId, function (error, deletedGroup) {
-            if (error) {
-                errorHandler.sendError(req, res, err);
-            }
-            else {
-                emailer.sendAuditMessageEMail('Group: "' + deletedGroup.title + '" was deleted by: ' + req.user.username);
-                return res.end();
-            }
-        })
-    }
+  // only admins can delete groups
+  if (!req.user || !req.user.hasRole('admin')) {
+    errorHandler.sendError(req, res, new Error('only admin users may delete a group'), 403);
+  }
+  // otherwise, get the group from the database then delete them
+  else {
+    groupsService.deleteGroup(groupDeleteId, function (error, deletedGroup) {
+      if (error) {
+        errorHandler.sendError(req, res, err);
+      }
+      else {
+        emailer.sendAuditMessageEMail('Group: "' + deletedGroup.title + '" was deleted by: ' + req.user.username);
+        return res.end();
+      }
+    })
+  }
 };
