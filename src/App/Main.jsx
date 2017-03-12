1ab9e5fee4e66c540492b7d5a26d4d66865ea230
Brandon Kreisel <kreiselb@gmail.com>
2017-03-12 15:52:59 -0400

    React Base Install

---
+++ src/App/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from '../Home/Home.jsx';
+
+const App = () => (
+  <Home />
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1> Hello Mosiac </h1>
+  </div>
+);
+
+export default Home;
