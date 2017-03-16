b4af5c79ee767b8b2b954a826925dba9d65d66af
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-03-16 18:36:06 +0100

    create semester schema/model

---
+++ server/models/Semester.js
@@ -0,0 +1,29 @@
+var logger = require('../config/logger');
+var mongoose = require('mongoose');
+
+var semesterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: '{PATH} is required!'
+    },
+    startDate: {
+        type: Date,
+        default: new Date()
+    },
+});
+
+var Semester = mongoose.model('Semester', semesterSchema);
+
+function createDefaultSemester() {
+    Semester.find({}).exec(function (err, collection) {
+        if (collection.length === 0) {
+            logger.log('creating default semester');
+            Semester.create({
+                name: 'Dummy Semester'
+            });
+        } else {
+            logger.log('not creating default settings because they are already created');
+        }
+    });
+}
+exports.createDefaultSettings = createDefaultSemester;
