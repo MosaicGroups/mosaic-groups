31bc11b9d975d3fc7a5d3f4de487f03c545bbca8
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-03-30 10:15:53 +0200

    have very basic router working - #97

--- client/src/App/Main.jsx
+++ client/src/App/Main.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from '../common/Header.jsx';
 
-import Home from '../Home/Home.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => (
-  <Home />
+  <Router history={Router.hashHistory}>
+    <div className="container">
+      <Header />
+      <Route path="/" component="" />
+      <Route path="/login" component="" />
+      <Route path="/profile" component="" />
+      <Route path="/group-create-or-edit" component="" />
+      <Route path="/group-create-or-edit/:id" component="" />
+      <Route path="/user-edit/:id" component="" />
+      <Route path="/user-list" component="" />
+      <Route path="/user-create" component="" />
+      <Route path="/group-list" component="" />
+      <Route path="/group-join/:id" component="" />
+      <Route path="/group-full/:id" component="" />
+    </div>
+  </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
