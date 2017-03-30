221c5047aa58bb49f9c9414405ba6842c05765eb
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-03-30 08:11:05 +0200

    update ui to capture campus and gender - #74

--- public/app/views/groupJoin/groupJoinCtrl.js
+++ public/app/views/groupJoin/groupJoinCtrl.js
@@ -1,40 +1,41 @@
-angular.module('app').controller('groupJoinCtrl', function ($scope, $route, $location, groupService, notifierService, identityService) {
+angular.module('app').controller('groupJoinCtrl', function ($scope, $route, $location, groupService, notifierService, identityService, genders, campuses) {
     var groupId = $route.current.params.id;
-
+    $scope.campuses = campuses;
+    $scope.genders = genders;
     $scope.disableJoin = false;
     $scope.groupIsFull = false;
     $scope.emailConfirmed = '';
     $scope.joinButtonTooltip = '';
 
     groupService.getGroup(groupId).$promise
-    .then(function (data) {
-        $scope.group = data;
-        $scope.group.newMember = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            status: 'PENDING'
-        };
-        $scope.groupIsFull = $scope.group.members.length >= $scope.group.memberLimit;
-        if ($scope.group.isForLeadersOnly() && !identityService.isAuthenticated()) {
-            $scope.disableJoin = true;
-            var errorMsg = 'You must be logged in if you want to join this group';
-            $scope.joinButtonTooltip = errorMsg;
-            notifierService.error(errorMsg);
-        }
-    });
+        .then(function (data) {
+            $scope.group = data;
+            $scope.group.newMember = {
+                firstName: '',
+                lastName: '',
+                email: '',
+                status: 'PENDING'
+            };
+            $scope.groupIsFull = $scope.group.members.length >= $scope.group.memberLimit;
+            if ($scope.group.isForLeadersOnly() && !identityService.isAuthenticated()) {
+                $scope.disableJoin = true;
+                var errorMsg = 'You must be logged in if you want to join this group';
+                $scope.joinButtonTooltip = errorMsg;
+                notifierService.error(errorMsg);
+            }
+        });
 
     $scope.joinGroup = function () {
         // if the form is valid then submit to the server
         if (!$scope.groupIsFull && !$scope.disableJoin) {
             $scope.disableJoin = true;
             groupService.addMember($scope.group)
-            .then(function () {
-                notifierService.notify('Your request to join "' + $scope.group.title + '" has been sent');
-                $location.path('/');
-            }, function () {
-                notifierService.error('Not able to join "' + $scope.group.title + '" at this time');
-            });
+                .then(function () {
+                    notifierService.notify('Your request to join "' + $scope.group.title + '" has been sent');
+                    $location.path('/');
+                }, function () {
+                    notifierService.error('Not able to join "' + $scope.group.title + '" at this time');
+                });
         }
     };
 });
