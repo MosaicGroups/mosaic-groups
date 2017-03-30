78bfc916ddc263a059c10c4de1996b1972aa1061
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-03-30 10:09:06 +0200

    reverted config and routes -- we do not want to update these when we 'refresh' the server files

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,16 +4,10 @@ var config = require('./config');
 var users = require('../controllers/usersController');
 var groups = require('../controllers/groupsController');
 var settings = require('../controllers/settingsController');
-var path = require('path');
 
-var indexRedirect = function (req, res) {
-    res.render('index', {
-        bootstrappedUser: req.user
-    });
-};
 
 // in production env, and if the proper headers are seen in the request, redirect to https
-var secureRedirect = function () {
+let secureRedirect = function () {
     return function (req, res, next) {
         if (process.env.NODE_ENV == 'production') {
             if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
@@ -29,22 +23,22 @@ var secureRedirect = function () {
                 next();
             }
         }
-    };
+    }
 };
 
 //routes
 module.exports = function (app) {
+
+
     app.get('/api/users/:id', cache.disableBrowserCache, auth.requiresRole('admin'), users.getUser);
     app.get('/api/users', cache.disableBrowserCache, users.getUsers);
     app.post('/api/users/:id', auth.requiresApiLogin, users.updateUser);
     app.delete('/api/users/:id', auth.requiresRole('admin'), users.deleteUser);
 
-    app.get('/api/groups/getSemester', cache.disableBrowserCache, auth.requiresRole('admin'), groups.currentSemester);
     app.get('/api/groups/:id', cache.disableBrowserCache, groups.getGroup);
     app.get('/api/groups', cache.disableBrowserCache, groups.getGroups);
     app.post('/api/groups/:id/add-member', settings.requiresGroupsEnabled, groups.addMember);
 
-    app.post('/api/groups/addSemester', cache.disableBrowserCache, auth.requiresRole('admin'), groups.addSemester);    
     app.post('/api/groups/emailGroupReportToSelf', cache.disableBrowserCache, auth.requiresRole('admin'), groups.emailGroupReportToSelf);
     app.post('/api/groups/emailUniqueReportToSelf', cache.disableBrowserCache, auth.requiresRole('admin'), groups.emailUniqueReportToSelf);
     app.post('/api/groups', cache.disableBrowserCache, auth.requiresApiLogin, groups.saveGroup);
@@ -56,21 +50,16 @@ module.exports = function (app) {
     app.get('/api/settings', cache.disableBrowserCache, settings.getSettings);
     app.post('/api/settings', auth.requiresRole('admin'), settings.updateSettings);
 
-    app.get('/partials/*', function (req, res) {
-        res.render('../../public/app/views/' + req.params[0]);
-    });
+   
 
     app.post('/login', auth.login);
 
     app.post('/logout', auth.logout);
 
-    // ensure that all requests to the login page get directed to the secure page instead
-    //  app.get('/login', secureRedirect(config), indexRedirect);
 
-    app.get('/login', indexRedirect);
-    app.get('/.well-known/*', function (req, res, next) {
-        res.sendFile(req.path, { root: path.normalize(__dirname + '/../../public/') });
-    });
+
     // ensure that the client side application does ALL of the routing
-    app.get('*', indexRedirect);
+    app.get('*', function (req, res) {
+        res.sendFile(req.path, config);
+    });
 };
