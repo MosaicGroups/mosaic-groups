68fd891464225a445fad5558a05060c738cc2887
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-03-31 10:35:12 +0200

    convert subheader to functional component

--- client/src/App/components/groups/list/SubHeader.jsx
+++ client/src/App/components/groups/list/SubHeader.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 
-
-class SubHeader extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    
-    render() {
-        let settings = this.props;
-        return (
-            <div className="jumbotron ">
-                {settings.showNextSemesterMsg ? <h2>{settings.nextSemesterMsg}</h2> : <span>Join a Mosaic Growth Group</span>}
-            </div>
-
-        );
-    }
-}
-
-
-
-
+const SubHeader = ({ settings }) => {
+    return (
+        <div className="jumbotron ">
+            {settings.showNextSemesterMsg ? <h2>{settings.nextSemesterMsg}</h2> : <span>Join a Mosaic Growth Group</span>}
+        </div>
+    );
+};
 export default SubHeader;
