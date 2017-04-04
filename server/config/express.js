f4d061c72a8d722d94ba8f6ab2a52743021d6ea9
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-04 08:12:49 +0200

    fix prod breaking problem

--- server/config/express.js
+++ server/config/express.js
@@ -4,9 +4,12 @@ let morgan = require('morgan');
 let bodyParser = require('body-parser');
 let cookieSession = require('cookie-session');
 let config = require('./config');
+let logger = require('./logger');
 
-let forceSsl = function (req, res, next) {
-    if (config.env === 'production' && !req.secure) {
+var forceSsl = function (req, res, next) {
+    logger.log("req.headers['x-forwarded-proto'] = " + req.headers['x-forwarded-proto']);
+    if (config.env === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+        logger.log("req.headers['x-forwarded-proto'] = " + req.headers['x-forwarded-proto']);
         return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     return next();
@@ -16,12 +19,11 @@ module.exports = function (app) {
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
     if (config.env !== 'test') {
-        let requestLogFormat = '\x1b[34mRequest:\x1b[0m [:date[iso]](:remote-addr): ":method :url HTTP/:http-version" :status :res[content-length]';
-        app.use(morgan(requestLogFormat));
+        app.use(morgan('combined'));
     }
     app.use(forceSsl);
     app.use(bodyParser());
-    app.use(cookieSession({secret: 'mosaic groups unicorns'}));
+    app.use(cookieSession({ secret: 'mosaic groups unicorns' }));
     app.use(passport.initialize());
     app.use(passport.session());
 
