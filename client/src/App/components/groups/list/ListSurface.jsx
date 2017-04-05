3392f2068083204af4b7f962efb2863d2ed6cbbc
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-05 13:56:53 +0200

    started VERY basic table for rendering groups

--- client/src/App/components/groups/list/ListSurface.jsx
+++ client/src/App/components/groups/list/ListSurface.jsx
@@ -2,6 +2,79 @@ import React from 'react';
 import SubHeader from './subHeader/SubHeader.jsx';
 import { connect } from 'react-redux';
 import { fetchSettingsIfNeeded, } from '../../../actions/settings';
+import { fetchGroupsIfNeeded, } from '../../../actions/groups';
+
+const Table = ({ groups }) => {
+    return (
+        <div className="container">
+            <table className="">
+                <thead >
+                    <tr >
+                        <th className="header ">
+                            Title
+      </th>
+                        <th className="header ">
+                            Details
+      </th>
+                        <th className="header ">Audience
+      </th>
+                        <th className="header ">
+                            Leader
+      </th>
+                        <th className="header ">
+                            Location
+      </th>
+                        <th className="header ">
+                            Time
+      </th>
+                        <th className="header ">Childcare
+      </th>
+                        <th className="header ">
+                            Topic
+      </th>
+                        <th className="header ">Action
+      </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        groups.map(group => {
+                            return (
+                                <tr className="row" key={group._id}>
+                                    <td>
+                                        {group.title}
+                                    </td>
+                                    <td>
+                                        {group.description}
+                                    </td>
+                                    <td>
+                                        {group.audienceType}
+                                    </td>
+                                    <td>
+                                    leaders
+                                    </td>
+                                    <td>
+                                        {group.location}
+                                    </td>
+                                    <td>
+                                        {group.meetingTime}
+                                    </td>
+                                    <td>
+                                        {group.childcare}
+                                    </td>
+                                    <td>
+                                     topics
+                                    </td>
+
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 class GroupListSurface extends React.Component {
     constructor(props) {
@@ -10,13 +83,16 @@ class GroupListSurface extends React.Component {
     componentDidMount() {
         const { dispatch } = this.props;
         dispatch(fetchSettingsIfNeeded());
+        dispatch(fetchGroupsIfNeeded());
     }
 
     render() {
+        let groups = this.props.groups || {};
         let settings = this.props.settings || {};
         return (
             <div>
-                {settings.hasSettings ? <SubHeader settings={this.props.settings} identity={this.props.identity}/> : null}
+                {settings.hasSettings ? <SubHeader settings={settings} identity={this.props.identity} /> : null}
+                {groups.hasGroups ? <Table groups={groups.groups} identity={this.props.identity} /> : null}
             </div>
         );
     }
@@ -28,7 +104,8 @@ GroupListSurface.propTypes = {
 const mapStateToProps = state => {
     return {
         settings: state.settings,
-        identity: state.identity
+        identity: state.identity,
+        groups: state.groups
     };
 };
 
