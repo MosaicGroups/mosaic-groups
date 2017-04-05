906646779c295194e17a87c005d6db0369062059
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-05 10:56:28 +0200

    base create form -- no user display yet

--- client/src/App/components/groups/createEdit/CreateEditForm.jsx
+++ client/src/App/components/groups/createEdit/CreateEditForm.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Well, FormGroup, ControlLabel } from 'react-bootstrap';
+import {
+    daysOfTheWeek,
+    meetingTimes,
+    audienceTypes,
+    availableTopics
+} from '../../../constants';
 
 const CheckboxGroup = ({ label, required, name, options, input, meta }) => (
     <FormGroup controlId={name}>
@@ -21,7 +27,7 @@ const CheckboxGroup = ({ label, required, name, options, input, meta }) => (
 
                             return input.onChange(newValue);
                         }} />
-                    {`${option.firstName} ${option.lastName}`} 
+                    {`${option.firstName} ${option.lastName}`}
                 </label>
             </div>))
         }
@@ -40,19 +46,57 @@ const Input = ({ label, children }) => {
 };
 
 const CreateEditForm = (props) => {
-    const { handleSubmit,  users } = props;
+    const { handleSubmit, users } = props;
     return (
         <div className="container">
             <Well>
                 <form name="createEditForm" className="form-horizontal" onSubmit={handleSubmit}>
                     <h3>Create/Edit Group</h3>
                     <Input label="Title">
-                        <Field component="input" name="title" type="text" placeholder="Title" required="required" autoComplete="off" className="form-control " />
+                        <Field component="input" name="title" type="text" placeholder="Title" required="required" autoComplete="off" className="form-control" />
                     </Input>
                     <Input label="Group Leader(s)">
                         <Field options={users} name="leaders" component={CheckboxGroup} />
-
-
+                    </Input>
+                    <Input label="City">
+                        <Field component="input" name="city" type="text" placeholder="City" required="required" autoComplete="off" className="form-control" />
+                    </Input>
+                    <Input label="Day Of The Week">
+                        <Field name="dayOfTheWeek" component="select" className="form-control">
+                            <option></option>
+                            {daysOfTheWeek.map(day => (<option key={day} value={day}>{day}</option>))}
+                        </Field>
+                    </Input>
+                    <Input label="Time">
+                        <Field name="meetingTime" component="select" className="form-control">
+                            <option></option>
+                            {meetingTimes.map(time => (<option key={time} value={time}>{time}</option>))}
+                        </Field>
+                    </Input>
+                    <Input label="Member Limit">
+                        <Field component="input" name="memberLimit" type="number" min="0" placeholder="20" required="required" autoComplete="off" className="form-control" />
+                    </Input>
+                    <Input label="Audience">
+                        <Field name="audienceType" component="select" className="form-control">
+                            <option></option>
+                            {audienceTypes.map(a => (<option key={a} value={a}>{a}</option>))}
+                        </Field>
+                        <div className="help-block">* select "Group Leaders" to create a group that is only visible when a group leader is logged in</div>
+                    </Input>
+                    <Input label="Childcare">
+                        <Field component="input" name="childcare" type="checkbox" />
+                    </Input>
+                    <Input label="Topic">
+                        <Field name="topic" component="select" className="form-control">
+                            <option></option>
+                            {availableTopics.map(t => (<option key={t} value={t}>{t}</option>))}
+                        </Field>
+                    </Input>
+                    <Input label="Description">
+                        <Field component="input" name="description" type="textarea" required="required" autoComplete="off" className="form-control" />
+                    </Input>
+                    <Input label="Group Is Disabled">
+                        <Field component="input" name="disabled" type="checkbox" />
                     </Input>
                 </form>
             </Well>
---
+++ client/src/App/constants/index.js
@@ -0,0 +1,77 @@
+exports.audienceTypes = [
+    'All',
+    'Couples',
+    'Group Leaders',
+    'High School Students',
+    'Married Couples',
+    'Men',
+    'Middle School Students',
+    'Parents',
+    'Women',
+    'Young Adults'
+];
+
+exports.availableTopics = [
+    'Bible Study',
+    'Book Study',
+    'Finance',
+    'Marriage/Relationship',
+    'Social'
+];
+
+exports.campuses = [
+    'Arundel', 'Elkridge'
+];
+
+exports.daysOfTheWeek = [
+    '6-Week Groups',
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+exports.genders = [
+    'Male',
+    'Female'
+];
+
+exports.meetingTimes = [
+    '6:00am',
+    '6:30am',
+    '7:00am',
+    '7:30am',
+    '8:00am',
+    '8:30am',
+    '9:00am',
+    '9:30am',
+    '10:00am',
+    '10:30am',
+    '11:00am',
+    '11:30am',
+    '12:00am',
+    '12:30pm',
+    '1:00pm',
+    '1:30pm',
+    '2:00pm',
+    '2:30pm',
+    '3:00pm',
+    '3:30pm',
+    '4:00pm',
+    '4:30pm',
+    '5:00pm',
+    '5:30pm',
+    '6:00pm',
+    '6:30pm',
+    '7:00pm',
+    '7:30pm',
+    '8:00pm'
+];
+
+exports.statusTypes = [
+    'APPROVED',
+    'PENDING'
+];
