f3b33af3aa0eb11d3a7d54807edfda02d4adfa0f
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-13 14:02:31 +0200

    move members form into own component

--- client/src/App/components/groups/createEdit/CreateEditForm.jsx
+++ client/src/App/components/groups/createEdit/CreateEditForm.jsx
@@ -8,77 +8,8 @@ import {
     availableTopics
 } from '../../../constants';
 import LeaderCheckboxGroup from './LeaderCheckboxGroup.jsx';
+import MembersForm from './MembersForm.jsx';
 
-
-const renderMembers = ({ fields, meta: { touched, error, submitFailed } }) => (
-    <div className="form-group">
-        <label htmlFor="members" className="col-md-2 control-label">Members</label>
-        <div className="col-md-10">
-            <table name="members" className="table table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {fields.map((member, index) =>
-                        <tr key={index}>
-                            <td>
-                                <Field
-                                    name={`${member}.firstName`}
-                                    type="text"
-                                    component="input"
-                                    placeholder="First Name"
-                                    className="form-control" />
-                            </td>
-                            <td>
-                                <Field
-                                    name={`${member}.lastName`}
-                                    type="text"
-                                    component="input"
-                                    placeholder="Last Name"
-                                    className="form-control" />
-                            </td>
-                            <td><Field
-                                name={`${member}.email`}
-                                component="input"
-                                type="text"
-                                placeholder="Email"
-                                className="form-control" />
-                            </td>
-                            <td style={{ width: '150px' }}>
-                                <Field name={`${member}.status`} required="required" value="PENDING" className="form-control" component="select">
-                                    <option value="PENDING" label="PENDING" >PENDING</option>
-                                    <option value="APPROVED" label="APPROVED">APPROVED</option>
-
-                                </Field>
-                            </td>
-                            <td><button onClick={() => fields.remove(index)} className="btn btn-primary">Remove</button></td>
-                        </tr>
-
-                    )}
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <button onClick={() => fields.push({})} className="btn btn-default">
-                                Add Member
-                        </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-
-);
 
 const Input = ({ label, children }) => {
     return (
@@ -146,7 +77,7 @@ const CreateEditForm = (props) => {
                     <Input label="Group Is Disabled">
                         <Field component="input" name="disabled" type="checkbox" />
                     </Input>
-                    <FieldArray name="leaders" component={renderMembers} />
+                    <FieldArray name="leaders" component={MembersForm} />
                     <div className="form-group">
                         <div className="col-md-10 col-md-offset-2">
                             <div className="pull-right">
---
+++ client/src/App/components/groups/createEdit/MembersForm.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Field } from 'redux-form';
+const MembersForm = ({ fields, meta: { touched, error, submitFailed } }) => (
+    <div className="form-group">
+        <label htmlFor="members" className="col-md-2 control-label">Members</label>
+        <div className="col-md-10">
+            <table name="members" className="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fields.map((member, index) =>
+                        <tr key={index}>
+                            <td>
+                                <Field
+                                    name={`${member}.firstName`}
+                                    type="text"
+                                    component="input"
+                                    placeholder="First Name"
+                                    className="form-control" />
+                            </td>
+                            <td>
+                                <Field
+                                    name={`${member}.lastName`}
+                                    type="text"
+                                    component="input"
+                                    placeholder="Last Name"
+                                    className="form-control" />
+                            </td>
+                            <td><Field
+                                name={`${member}.email`}
+                                component="input"
+                                type="text"
+                                placeholder="Email"
+                                className="form-control" />
+                            </td>
+                            <td style={{ width: '150px' }}>
+                                <Field name={`${member}.status`} required="required" value="PENDING" className="form-control" component="select">
+                                    <option value="PENDING" label="PENDING" >PENDING</option>
+                                    <option value="APPROVED" label="APPROVED">APPROVED</option>
+
+                                </Field>
+                            </td>
+                            <td><button onClick={() => fields.remove(index)} className="btn btn-primary">Remove</button></td>
+                        </tr>
+
+                    )}
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <button onClick={() => fields.push({})} className="btn btn-default">
+                                Add Member
+                        </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+);
+
+export default MembersForm
