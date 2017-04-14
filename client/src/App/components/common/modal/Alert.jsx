5397cd08c76e5124f29f9fd13085817d1c8fc35b
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-14 15:15:58 +0200

    add modal dialoge framework

---
+++ client/src/App/components/common/modal/Alert.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import show from './show.js';
+class Alert extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { show: false };
+        this.show = show.bind(this);
+    }
+
+    render() {
+
+        return (
+            <Modal show={this.state.show}>
+                <Modal.Header>
+                    <Modal.Title>Alert</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    {this.props.children}
+                </Modal.Body>
+
+                <Modal.Footer>
+
+                    <Button bsStyle="primary" onClick={this.accept}>
+                        Ok
+                    </Button>
+                </Modal.Footer>
+
+            </Modal>
+        );
+    }
+}
+export default Alert;
