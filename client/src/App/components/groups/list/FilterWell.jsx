065c5be2634f06f3d09cd0dbbb9e82d69f059c79
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-17 10:35:40 +0200

    have text based field filtering working

--- client/src/App/components/groups/list/FilterWell.jsx
+++ client/src/App/components/groups/list/FilterWell.jsx
@@ -14,25 +14,38 @@ const FilterElement = ({ label, children }) => {
     );
 };
 
+
+
 class FilterWell extends React.Component {
     constructor(props) {
         super(props);
-
+        this.doFilter = this.doFilter.bind(this);
     }
+    doFilter(key, filterString) {
+        let groups = this.props.groups || [];
+
+        if (filterString || filterString.length > 0) {
+            this.props.updateFilter(groups.filter(g => g[key].toLowerCase().includes(filterString.toLowerCase())));
+        }
+        else {
+            this.props.updateFilter(groups);
+        }
+    }
+
     render() {
-        let groups = this.props.groups|| [];
-        let updateFilter = this.props.updateFilter;
         return (<Well>
             <Row>
                 <div className="h3">Filter The Groups By:</div>
             </Row>
             <FilterElement label="Title">
                 <input name="titleFilter" placeholder="Filter Title" type="text" autoComplete="off" className="form-control" onChange={(e) => {
-                    let value = e.target.value;
-
-                    value ?
-                        updateFilter(groups.filter(g => g.title.includes(value))) : updateFilter(groups);
-                }}/>
+                    this.doFilter('title', e.target.value);
+                }} />
+            </FilterElement>
+            <FilterElement label="Location">
+                <input name="locationFilter" placeholder="Filter Location" type="text" autoComplete="off" className="form-control" onChange={(e) => {
+                    this.doFilter('location', e.target.value);
+                }} />
             </FilterElement>
         </Well>);
     }
