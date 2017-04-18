fcbac0da6d169a3db369232b64d03731f1e6afdf
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-18 06:53:14 +0200

    add multi-select filter

--- client/src/App/components/groups/list/FilterWell.jsx
+++ client/src/App/components/groups/list/FilterWell.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Well, Row, Col } from 'react-bootstrap';
-
+import Multiselect from 'react-bootstrap-multiselect';
+import 'react-bootstrap-multiselect/css/bootstrap-multiselect.css';
+import {
+    daysOfTheWeek
+} from '../../../constants';
 const FilterElement = ({ label, children }) => {
     return (
         <Row>
@@ -14,25 +18,49 @@ const FilterElement = ({ label, children }) => {
     );
 };
 
-
-
 class FilterWell extends React.Component {
     constructor(props) {
         super(props);
         this.doFilter = this.doFilter.bind(this);
-    }
-    doFilter(key, filterString) {
-        let groups = this.props.groups || [];
+        this.daysOfTheWeekOptions = daysOfTheWeek.map(d => {
+            return { value: d };
+        });
+        this.dotwChecked = [];
+        this.filters = new Map();
 
-        if (filterString || filterString.length > 0) {
-            this.props.updateFilter(groups.filter(g => g[key].toLowerCase().includes(filterString.toLowerCase())));
+
+    }
+    doFilter(key, filterValue) {
+
+        let filterdGroups = this.props.groups || [];
+        // if a key is passed along with an empty value, then we need to delete that filter
+        if (!filterValue || (filterValue && filterValue.length == 0)) {
+            this.filters.delete(key);
         }
-        else {
-            this.props.updateFilter(groups);
+        // if the filter is matching an array --
+        else if (Array.isArray(filterValue)) {
+            this.filters.set(key, key => g => {
+
+                return filterValue.filter(element => g[key].toLowerCase() === element.toLowerCase()).length > 0;
+            });
         }
+        // if the filter is a string    
+        else if (filterValue.length > 0) {
+            this.filters.set(key, key => g => g[key].toLowerCase().includes(filterValue.toLowerCase()));
+        }
+
+        // apply all filters
+        this.filters.forEach((value, key) => {
+            filterdGroups = filterdGroups.filter(value(key));
+        });
+
+
+        this.props.updateFilter(filterdGroups);
+
     }
 
     render() {
+        let dotwChecked = this.dotwChecked;
         return (<Well>
             <Row>
                 <div className="h3">Filter The Groups By:</div>
@@ -46,6 +74,24 @@ class FilterWell extends React.Component {
                 <input name="locationFilter" placeholder="Filter Location" type="text" autoComplete="off" className="form-control" onChange={(e) => {
                     this.doFilter('location', e.target.value);
                 }} />
+            </FilterElement>
+            <FilterElement label="Day">
+                <Multiselect
+                    onChange={(option, checked) => {
+                        // if the checkbox is being checked, and its not already in the list, then add it
+                        if (checked && !dotwChecked.includes(option.val())) {
+                            dotwChecked.push(option.val());
+
+                        }
+                        // otherwise, we are removing it
+                        else {
+                            dotwChecked = dotwChecked.filter(day => day !== option.val());
+                        }
+                        this.doFilter('dayOfTheWeek', dotwChecked);
+
+                    }}
+                    data={this.daysOfTheWeekOptions} multiple />
+
             </FilterElement>
         </Well>);
     }
