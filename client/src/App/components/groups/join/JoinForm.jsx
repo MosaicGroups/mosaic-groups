9596989aa76db1898a7ea9ee7841ee50c67c93d3
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-04-22 14:01:18 +0200

    Basic Join Form (no special cases)

--- client/src/App/components/groups/join/JoinForm.jsx
+++ client/src/App/components/groups/join/JoinForm.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Well, } from 'react-bootstrap';
+import { Well, Col } from 'react-bootstrap';
 import InputRow from '../../common/InputRow.jsx';
+import {
+    campuses,
+    genders
+} from '../../../constants';
 
 const JoinForm = (props) => {
 
@@ -12,24 +16,51 @@ const JoinForm = (props) => {
             <form name="joinForm" className="form-horizontal" onSubmit={handleSubmit}>
 
                 <InputRow label="First Name">
-                    <Field component="input" name="title" type="text" placeholder="Title" required="required" autoComplete="off" className="form-control" />
+                    <Field component="input" name="firstName" type="text" placeholder="First Name" required="required" autoComplete="off" className="form-control" />
                 </InputRow>
 
                 <InputRow label="Last Name">
-                    <Field component="input" name="location" type="text" placeholder="City" required="required" autoComplete="off" className="form-control" />
+                    <Field component="input" name="lastName" type="text" placeholder="Last Name" required="required" autoComplete="off" className="form-control" />
                 </InputRow>
 
                 <InputRow label="Email">
-                    <Field component="input" name="childcare" type="checkbox" />
+                    <Field component="input" name="email" type="email" placeholder="Email" required="required" autoComplete="off" className="form-control" />
                 </InputRow>
 
                 <InputRow label="Retype Email">
-                    <Field component="input" name="description" type="textarea" required="required" autoComplete="off" className="form-control" />
+                    <Field component="input" name="emailConfirmed" placeholder="Retype Email" type="email" required="required" autoComplete="off" className="form-control" />
                 </InputRow>
-                <InputRow label="Group Is Disabled">
-                    <Field component="input" name="disabled" type="checkbox" />
+                <InputRow label="Gender">
+                    <Field name="gender" component="select" className="form-control">
+                        <option></option>
+                        {genders.map(g => (<option key={g} value={g}>{g}</option>))}
+                    </Field>
                 </InputRow>
+                <InputRow label="Campus">
+                    <Field name="campus" component="select" className="form-control">
+                        <option></option>
+                        {campuses.map(g => (<option key={g} value={g}>{g}</option>))}
+                    </Field>
+                </InputRow>
+                <InputRow label="Phone Number">
+                    <Field name="phone" component="input" type="text" placeholder="Phone Number" required="required" autoComplete="off" className="form-control" />
+                </InputRow>
+                <InputRow label="Preferred Contact Method">
+                    <Col md={6}>
+                        <label className="radio-inline">
+                            <Field name="preferContactVia" component="input" type="radio" value="phone" />
 
+                            Phone
+                        </label>
+                    </Col>
+                    <Col md={6}>
+                        <label className="radio-inline">
+                            <Field name="preferContactVia" component="input" type="radio" value="email" />
+
+                            Email
+                        </label>
+                    </Col>
+                </InputRow>
             </form>
         </Well>
     );
