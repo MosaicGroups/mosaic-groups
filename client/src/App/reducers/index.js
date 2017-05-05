b5fa3e0653463acf6e0bcae2e161e641df6bc200
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-05-05 08:28:08 +0200

    have basic toast working for logins

--- client/src/App/reducers/index.js
+++ client/src/App/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
+import { reducer as toastr } from 'react-redux-toastr';
 import settings from './settings';
 import identity from './identity';
 import users from './users';
 import groups from './groups';
 
 const rootReducer = combineReducers({
+    toastr,
     settings,
     identity,
     users,
