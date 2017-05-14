cd0ff794701649882a6259106ef7872cf7991f33
Brandon Kreisel <kreiselb@gmail.com>
2017-05-14 15:13:50 -0400

    New Semester Components

--- client/src/App/components/groups/list/AdminWell.jsx
+++ client/src/App/components/groups/list/AdminWell.jsx
@@ -1,62 +1,79 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Well, Row, Col, Button } from 'react-bootstrap';
 import * as request from 'superagent';
 import { toastr } from 'react-redux-toastr';
-import { connect } from 'react-redux';
 import { updateSettings } from '../../../actions/settings.js';
-const AdminWell = ({ settings, dispatch }) => (
-    <Well>
-        <Row>
-            <div className="h3">Admin</div>
-        </Row>
-        <Row>
-            <Col md={12}>
-                <Button onClick={(e) => {
-                    request.post('/api/groups/emailGroupReportToSelf')
-                        .then(res => {
-                            toastr.success('Success', 'Group Report email sent');
-                        })
-                        .catch(err => {
-                            toastr.success('Error', 'There was an error sending the Group report');
-                        });
-                }}
-                >Email Me Group Report </Button>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={12}>
-                <Button onClick={(e) => {
-                    request.post('/api/groups/emailUniqueReportToSelf')
-                        .then(res => {
-                            toastr.success('Success', 'Unique Members Report email sent');
-                        })
-                        .catch(err => {
-                            toastr.success('Error', 'There was an error sending the Unique Members report');
-                        });
-                }}
-                >Email Me Unique Members Report </Button>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={12}>
-                <Button onClick={(e) => {
-                    dispatch(updateSettings('disableGroups', !settings.disableGroups));
-                }}
-                >{settings.disableGroups ? 'Enable' : 'Disable'} Groups</Button><br />
-                <i>- group signup is currently {settings.disableGroups ? 'disabled' : 'enabled'}</i>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={12}>
-                <Button onClick={(e) => {
-                    dispatch(updateSettings('showNextSemesterMsg', !settings.showNextSemesterMsg));
-                }}
-                >{settings.showNextSemesterMsg ? 'Hide' : 'Show'} Next Semester Message</Button><br />
-                <i>- group list is currently being {settings.showNextSemesterMsg ? 'hidden from' : 'shown to'} users</i><br />
-                {settings.showNextSemesterMsg ?<i>- update the message on the right</i> : null}
-            </Col>
-        </Row>
-    </Well>
-);
+import { startNewSemester } from '../../../actions/groups.js';
+import  SemesterForm from './SemesterForm.jsx';
+
+const rowStyle = {
+    'marginTop' : '15px'
+};
+
+const AdminWell = ({ settings, dispatch }) => {
+
+    let newSemsterSubmit = ({semesterName}) => {
+        dispatch(startNewSemester(semesterName));
+    };  
+    
+    return (
+        <Well>
+            <Row style={rowStyle}>
+                <div className="h3">Admin</div>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <Button onClick={(e) => {
+                        request.post('/api/groups/emailGroupReportToSelf')
+                            .then(res => {
+                                toastr.success('Success', 'Group Report email sent');
+                            })
+                            .catch(err => {
+                                toastr.success('Error', 'There was an error sending the Group report');
+                            });
+                    }}
+                    >Email Me Group Report </Button>
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={12}>
+                    <Button onClick={(e) => {
+                        request.post('/api/groups/emailUniqueReportToSelf')
+                            .then(res => {
+                                toastr.success('Success', 'Unique Members Report email sent');
+                            })
+                            .catch(err => {
+                                toastr.success('Error', 'There was an error sending the Unique Members report');
+                            });
+                    }}
+                    >Email Me Unique Members Report </Button>
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={12}>
+                    <Button onClick={(e) => {
+                        dispatch(updateSettings('disableGroups', !settings.disableGroups));
+                    }}
+                    >{settings.disableGroups ? 'Enable' : 'Disable'} Groups</Button><br />
+                    <i>- group signup is currently {settings.disableGroups ? 'disabled' : 'enabled'}</i>
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={12}>
+                    <Button onClick={(e) => {
+                        dispatch(updateSettings('showNextSemesterMsg', !settings.showNextSemesterMsg));
+                    }}
+                    >{settings.showNextSemesterMsg ? 'Hide' : 'Show'} Next Semester Message</Button><br />
+                    <i>- group list is currently being {settings.showNextSemesterMsg ? 'hidden from' : 'shown to'} users</i><br />
+                    {settings.showNextSemesterMsg ?<i>- update the message on the right</i> : null}
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <SemesterForm onSubmit={newSemsterSubmit}/>
+            </Row>
+        </Well>
+    );
+};
 
 export default connect()(AdminWell);
---
+++ client/src/App/components/groups/list/SemesterForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Col, Button } from 'react-bootstrap';
+
+const SemesterForm = ({ handleSubmit, pristine, submitting }) => {
+
+    const semesterStyle = {
+        'marginBottom' : '10px',
+        'width' : '80%'
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="form-horizontal">
+            <Col md={12}>
+                <Field
+                  name="semesterName"
+                  component="input"
+                  type="text"
+                  placeholder="New Semester Name"
+                  className="form-control"
+                  style={semesterStyle}
+                />
+            </Col>
+            <Col md={12}>
+                <Button type="submit" disabled={pristine || submitting}
+                >Start New Semester</Button>
+            </Col>
+        </form>
+    );
+};
+
+export default reduxForm({
+    form: 'newSemesterForm',
+})(SemesterForm);
