2d38167dc1252f791d0d8e5ed65161988b2cb7f4
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-05-17 09:50:40 +0200

    change order and wording of links

--- client/src/App/components/groups/list/ActionLinks.jsx
+++ client/src/App/components/groups/list/ActionLinks.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { groupDisabled, groupIsFull, userCanEditGroup } from '../../../utils/index.js';
-import {  Row , Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 const ActionLinks = ({ identity, group, settings }) => {
     return (<Row className="text-right">
-        {userCanEditGroup(group, identity) ? <Col md={12}><a href={`/group/createEdit/${group._id}`}>Edit</a> </Col>: null}
         {
-            !groupIsFull(group) && !groupDisabled(group, settings) ?  <Col md={12}><a href={`/group/join/${group._id}`}>Join</a> </Col>: (
-                (groupDisabled(group, settings) ? <Col md={12}> <b>Group is Disabled</b> </Col>:
-                    groupIsFull(group) ?  <Col md={12}><b>Group is Full</b> </Col>: null)
+            !groupIsFull(group) && !groupDisabled(group, settings) ? <Col md={12}><a href={`/group/join/${group._id}`}>Join</a> </Col> : (
+                (groupDisabled(group, settings) ? <Col md={12}> <b>Group is Disabled</b> </Col> :
+                    groupIsFull(group) ? <Col md={12}><b>Group is Full</b> </Col> : null)
             )
         }
-         <Col md={12}> {`(${group.members.length} of ${group.memberLimit})`}</Col>
+        {userCanEditGroup(group, identity) ? <Col md={12}><a href={`/group/createEdit/${group._id}`}>Manage</a> </Col> : null}
+
+        <Col md={12}> {`(${group.members.length} of ${group.memberLimit})`}</Col>
     </Row>);
 };
 
