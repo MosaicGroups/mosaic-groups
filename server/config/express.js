e9942acf025c5fb6f3623df6ca942efcdf842192
Megan Mercer <megan.mercer@inovexcorp.com>
2018-01-04 09:23:46 -0500

    Try manual setting of CORS headers.

--- server/config/express.js
+++ server/config/express.js
@@ -29,7 +29,24 @@ module.exports = function (app) {
     app.use(passport.session());
 
     if (config.env === 'production') {
-        app.use(cors({origin: 'http://mosaicchristian-org.s3-website-us-east-1.amazonaws.com'}));
+        // app.use(cors({origin: 'http://mosaicchristian-org.s3-website-us-east-1.amazonaws.com'}));
+        app.use(function (req, res, next) {
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'http://mosaicchristian-org.s3-website-us-east-1.amazonaws.com');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+
+            // Pass to next layer of middleware
+            next();
+        });
     } else {
         app.set('views', config.rootPath + '/server/views');
 
